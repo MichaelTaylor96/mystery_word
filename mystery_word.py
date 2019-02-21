caf43c7059b2111bace56c862520a445f2a45435
MichaelTaylor96 <68taylor@cua.edu>
2019-02-21 14:54:40 -0500

    Can play multiple times

--- mystery_word.py
+++ mystery_word.py
@@ -7,7 +7,8 @@ hard_words = []
 game_mode = ""
 mystery_word = ""
 current_letters = []
-guesses = 8
+guesses_left = 8
+guesses = []
 
 with open("words.txt") as all_words:
         line = all_words.readline()
@@ -44,25 +45,52 @@ while running:
     print('')
     print(cl_print)
     print('')
-    print("You have", guesses, "more lives")
+    print("You have", guesses_left, "more lives")
 
     if cl_print.replace(" ", "") == mystery_word:
-        print("You Win!")
+        print('')
+        print("You Win!") 
         running = False
-        break
+        # break
 
     guess = input("Guess a letter: ").upper()
 
-    if guess not in mystery_word:
-        print("Nope!")
-        guesses -= 1
-        if guesses == 0:
-            print("You Lose!")
-            running = False
-    
+    if guess not in guesses:
+        guesses.append(guess)
+
+        if guess not in mystery_word:
+            print('')
+            print("Nope!")
+            guesses_left -= 1
+            if guesses_left == 0:
+                print("You Lose!")
+                running = False
+        
+        else:
+            i = 0
+            while i < len(mystery_word):
+                if mystery_word[i] == guess:
+                    current_letters[i] = f"{guess} "
+                i += 1
+
     else:
-        i = 0
-        while i < len(mystery_word):
-            if mystery_word[i] == guess:
-                current_letters[i] = f"{guess} "
-            i += 1
+        print('')
+        print("You already guessed that letter.")
+
+    if not running:
+        if input("Would you like to play again? (Y or N): ").upper() == "Y":
+                mode_choice = input("Select difficulty (Easy, Normal, or Hard): ")
+                mode_choice = mode_choice.lower()
+                game_mode = mode_choice
+
+                if game_mode == "easy":
+                    mystery_word = easy_words[randrange(len(easy_words))].upper()
+                if game_mode == "normal":
+                    mystery_word = normal_words[randrange(len(normal_words))].upper()
+                if game_mode == "hard":
+                    mystery_word = hard_words[randrange(len(hard_words))].upper()
+
+                current_letters = []
+                guesses_left = 8
+                guesses = []
+                running = True
